c97e2c3e25879fc822f435dd49d7b0b0cc2f6692
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-02 12:13:18 +0100

    Fix script tag typo and character image path

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,15 +1,19 @@
 
 class MovableObject {
-    x; 120;
-    y; 400;
+    x = 120;
+    y = 400;
     img;
     
+    loadImage (path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
 
     moveRight () {
         console.log("move right");
     }
 
     moveLeft () {
-        
+
     }
 }
