586d2996371c5b120a2d1d05e917969157e62b5a
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-02 11:19:50 +0100

    feat: add a model folder

--- js/game.js
+++ js/game.js
@@ -1,18 +1,14 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    character.onload = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(character, 20, 20, 50, 150);
+
+console.log("my Charachter is", character);
     };
-    character.onerror = () => {
-        console.error("Bild konnte nicht geladen werden:", character.src);
-    };
-    character.src = "img/2_character_pepe/1_idle/idle/I-1.png";
-}
+
+    
