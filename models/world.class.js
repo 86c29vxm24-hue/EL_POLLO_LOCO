5e596660e40e88c215003f630b9dce6a5eb9d601
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-02 12:33:00 +0100

    Draw character after image load

--- models/world.class.js
+++ models/world.class.js
@@ -1,24 +1,25 @@
+class World {
+  character = new Character();
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  ctx;
 
-class World { 
-
-
-character = new Character();
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken()
-];
-
-
-
-    draw() {
-
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    if (this.character?.img) {
+      this.character.img.onload = () => this.draw();
+      if (this.character.img.complete) {
+        this.draw();
+      }
     }
+  }
 
-
-
+  draw() {
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height,
+    );
+  }
 }
-
-
-
-
