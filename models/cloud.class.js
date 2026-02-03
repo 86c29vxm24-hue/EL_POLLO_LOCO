3cd3be9628abbfd85bb488ec747c040635b7c0a3
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-03 09:30:58 +0100

    cloud moving left

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -10,8 +10,13 @@ class Cloud extends MovableObject {
         this.loadImage("img/5_background/layers/4_clouds/1.png");
 
         this.x = Math.random() * 500;
-       
-
+        this.animate();
     }
 
-}
+        animate() {
+            setInterval(() => {
+                this.x -= 0.15;
+            }, 1000 / 60);
+        }
+    }
+            
