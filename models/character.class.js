81b84e043d3f2df8d150da9ac4bfec24fa7c6d4f
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-03 10:42:37 +0100

    fixed animation character speed

--- models/character.class.js
+++ models/character.class.js
@@ -23,10 +23,10 @@ class Character extends MovableObject {
   animate() {
     setInterval(() => {
     let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[this.currentImage];
+      let path = this.IMAGES_WALKING[i];
       this.img = this.imageCache[path];
       this.currentImage++;
-    }, 1000);
+    }, 100);
   }
 
   jump() {}
