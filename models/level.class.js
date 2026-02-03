355fb323ebc0afd53f95786f5ce2ec54e30022e5
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-03 13:24:01 +0100

    added a level class and a level 1 js to put the array from the world to the level1 js

---
+++ models/level.class.js
@@ -0,0 +1,10 @@
+class Level {
+  enemies;
+  Clouds;
+  backgroundObjects;
+
+    constructor(enemies, clouds, backgroundObjects) {
+      this.enemies = enemies;
+      this.Clouds = clouds;
+      this.backgroundObjects = backgroundObjects;
+}}
