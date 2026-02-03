b5c1c9130de826bd0c10998d52294664f5d4833f
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-03 10:51:24 +0100

    chicken walking animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,20 +3,31 @@ class Chicken extends MovableObject {
 y = 370;
 height = 60;
 width = 80;
+IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+];
+
+
 
        constructor() {
         super();
         this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        this.loadImages(this.IMAGES_WALKING);
 
         this.x = 200 + Math.random() * 500; 
         this.animate();
     }
 
         animate() {
-            setInterval(() => {
-                this.x -= 0.5;
-            }, 1000 / 60);
-        }
+    setInterval(() => {
+    let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 200);
+  }
 
 
 }
