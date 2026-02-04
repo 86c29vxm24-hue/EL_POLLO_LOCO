eebbc102e05d9e9a718efd318bd658aefccfaa3b
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-04 12:01:32 +0100

    added new classes (drawable-object & status-bar)

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,11 +1,5 @@
-class MovableObject {
-  x = 120;
-  y = 280;
-  img;
-  width = 100;
-  height = 150;
-  imageCache = {};
-  currentImage = 0;
+class MovableObject extends DrawableObject {
+
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -13,14 +7,7 @@ class MovableObject {
   energy = 100;
   lastHit = 0;
 
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
 
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
 
   drawFrame(ctx) {
     if (this instanceof Character || this instanceof Chicken) {
@@ -46,17 +33,7 @@ class MovableObject {
     return this.y < 160;
   }
 
-  /**
-   *
-   * @param {Array} arr
-   */
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
+
 
   moveRight() {
     this.x += this.speed;
