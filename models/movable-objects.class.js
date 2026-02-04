ff78f57caed38414023e04c4587da660cc1160a6
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-04 11:49:32 +0100

    Hurt animation

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,103 +1,107 @@
-
 class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    width = 100;
-    height = 150;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15;
-    otherDirection = false;
-    speedY = 0;
-    acceleration = 2.5;
-    energy = 100;
-    
-    loadImage (path) {
-        this.img = new Image();
-        this.img.src = path;
+  x = 120;
+  y = 280;
+  img;
+  width = 100;
+  height = 150;
+  imageCache = {};
+  currentImage = 0;
+  speed = 0.15;
+  otherDirection = false;
+  speedY = 0;
+  acceleration = 2.5;
+  energy = 100;
+  lastHit = 0;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "4";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
     }
+  }
 
-    draw (ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+      1000 / 25;
+    });
+  }
+
+  isAboveGround() {
+    return this.y < 160;
+  }
+
+  /**
+   *
+   * @param {Array} arr
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
+  moveRight() {
+    this.x += this.speed;
+  }
+
+  moveLeft() {
+    {
+      this.x -= this.speed;
     }
+  }
 
-    drawFrame (ctx) {
-        if (this instanceof Character || this instanceof Chicken) {
-        ctx.beginPath();
-        ctx.lineWidth = "4";
-        ctx.strokeStyle = "blue";
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
-    }}
+  playAnimation(images) {
+    let i = this.currentImage % images.length;
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
 
-    applyGravity () {
-        setInterval(() => {
-            if (this.isAboveGround() || this.speedY > 0) {
-                this.y -= this.speedY
-                this.speedY -= this.acceleration;
-            } 1000 / 25});
-    }
+  jump() {
+    this.speedY = 30;
+  }
 
-    isAboveGround() {
-        return this.y < 160;
-    }
-
-
-   
-    /**
-     * 
-     * @param {Array} arr 
-     */
-    loadImages (arr) {
-        arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;        
-        this.imageCache[path] = img;
-        });
-    }
-
-  
-    moveRight () {   
-                this.x += this.speed;
-            
-        }
-
-       moveLeft () {   {
-                this.x -= this.speed;
-               
-        }}
-
-        playAnimation (images) {
-            let i = this.currentImage % images.length;
-            let path = images[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }
-
-
-jump() {
-  this.speedY = 30;
-}
-
-isColliding(mo) {
-  return (
-    this.x + this.width > mo.x &&
-    this.x < mo.x + mo.width &&
-    this.y + this.height > mo.y &&
-    this.y < mo.y + mo.height
-  );}   
-
+  isColliding(mo) {
+    return (
+      this.x + this.width > mo.x &&
+      this.x < mo.x + mo.width &&
+      this.y + this.height > mo.y &&
+      this.y < mo.y + mo.height
+    );
+  }
 
   hit() {
     this.energy -= 5;
     if (this.energy < 0) {
       this.energy = 0;
-    }
+    } else {this.lastHit = new Date().getTime();}
+  }
+
+  isHurt() {
+    let actualTime = new Date().getTime();
+    let timePassed = actualTime - this.lastHit;
+    return timePassed < 1000;
   }
 
   isDead() {
     return this.energy == 0;
   }
-
 }
