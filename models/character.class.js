8e3c517c4695f558f755d75306068e90d7ed82a9
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-04 08:31:39 +0100

    added the endboss

--- models/character.class.js
+++ models/character.class.js
@@ -46,10 +46,7 @@ class Character extends MovableObject {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
             this.x += this.speed;
 
-    let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
+    this.playAnimation(this.IMAGES_WALKING);
     }
     }, 50);
   }
