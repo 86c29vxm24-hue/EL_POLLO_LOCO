0eaa7bde991482acd0851765f8027ca795e97d53
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-06 11:05:47 +0100

    Add splash animation on bottle impact

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -6,14 +6,23 @@ class ThrowableObject extends MovableObject {
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
   ];
 
+  IMAGE_BOTTLESPLASH = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+  ];
+
   constructor(x, y, groundY) {
     super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
     this.loadImages(this.IMAGE_BOTTLETHROW);
+    this.loadImages(this.IMAGE_BOTTLESPLASH);
     this.x = x;
     this.y = y;
     this.width = 50;
     this.height = 60;
     this.groundY = groundY;
+    this.hasSplashed = false;
     this.throw();
   }
 
@@ -21,14 +30,19 @@ class ThrowableObject extends MovableObject {
     this.speedY = 15;
     this.applyGravity();
     let animationInterval = setInterval(() => {
-      this.playAnimation(this.IMAGE_BOTTLETHROW);
+      if (!this.hasSplashed) {
+        this.playAnimation(this.IMAGE_BOTTLETHROW);
+      }
     }, 100);
     let throwInterval = setInterval(() => {
-      this.x += 8;
+      if (!this.hasSplashed) {
+        this.x += 8;
+      }
       if (!this.isAboveGround()) {
         this.y = this.groundY;
         this.speedY = 0;
         clearInterval(animationInterval);
+        this.splash();
         clearInterval(throwInterval);
       }
     }, 1000 / 60);
@@ -36,5 +50,17 @@ class ThrowableObject extends MovableObject {
 
   isAboveGround() {
     return this.y < this.groundY;
+  }
+
+  splash() {
+    this.hasSplashed = true;
+    let i = 0;
+    let splashInterval = setInterval(() => {
+      this.img = this.imageCache[this.IMAGE_BOTTLESPLASH[i]];
+      i++;
+      if (i >= this.IMAGE_BOTTLESPLASH.length) {
+        clearInterval(splashInterval);
+      }
+    }, 100);
   }
 }
