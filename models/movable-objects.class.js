4a893c81442dd0cbda21cbbe08c8054d53b1ea33
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-09 18:27:50 +0100

    Increase hit damage to 20% per collision

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -88,7 +88,7 @@ class MovableObject extends DrawableObject {
   }
 
   hit() {
-    this.energy -= 5;
+    this.energy -= 20;
     if (this.energy < 0) {
       this.energy = 0;
     } else {this.lastHit = new Date().getTime();}
