afcb23686fcba5a80d379ccf3ccf431f68c93812
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-09 14:07:03 +0100

    Added Long Idle Images

--- models/character.class.js
+++ models/character.class.js
@@ -39,6 +39,19 @@ class Character extends MovableObject {
     "img/2_character_pepe/4_hurt/H-43.png",
   ];
 
+  IMAGE_LONG_IDLE =[
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png"
+  ]
+
   currentImage = 0;
 
   constructor() {
