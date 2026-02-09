508c46ff0295eefb2934c699d5602df6d31e4957
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-09 14:15:18 +0100

    added start and end screen

---
+++ models/end-screen.class.js
@@ -0,0 +1,14 @@
+class endScreen extends DrawableObject {
+
+IMAGES_YOU_WIN =[
+    "img/You won, you lost/You Win A.png"
+]
+
+
+IMAGES_GAME_OVER =[
+    "img/You won, you lost/Game Over.png"
+]
+
+
+
+}
---
+++ models/start-screen.class.js
@@ -0,0 +1,9 @@
+class Sart extends DrawableObject {
+
+    IMAGE_START_SCREEN = [
+        "img/9_intro_outro_screens/start/startscreen_1.png"
+    ]
+
+
+
+}
