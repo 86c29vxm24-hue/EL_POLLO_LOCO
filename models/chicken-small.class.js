771f356accc2b95e549a67f000e5548cabefc329
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-10 11:38:08 +0100

    added small chicken

---
+++ models/chicken-small.class.js
@@ -0,0 +1,16 @@
+class chickenSmall extends MovableObject {
+
+    IMAGE_WALK = [
+        "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/3_w.png"
+     ]
+
+
+     IMAGE_DEAD = [
+        "img/3_enemies_chicken/chicken_small/2_dead/dead.png"
+    ]
+
+
+
+}
