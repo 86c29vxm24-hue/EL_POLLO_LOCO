67275a69b91773b67ddf7777704c0711965f8780
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-02-11 16:09:59 +0100

    chicken-small class deleted

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -101,4 +101,27 @@ class Chicken extends ChickenEnemy {
     });
   }
 }
-  
+
+class ChickenSmall extends ChickenEnemy {
+  /**
+    * Initializes the instance.
+    *
+   * @returns {void}
+   */
+  constructor() {
+    super({
+      y: 380,
+      height: 50,
+      width: 60,
+      image: "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+      walkingImages: [
+        "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+      ],
+      deadImages: ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"],
+      speedMin: 0.2,
+      speedRange: 0.55,
+    });
+  }
+}
